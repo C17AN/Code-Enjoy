7bc8a6e657a2a496395f2c7c8bfa6efd2f0a96f6
c17an <kimchanmin1@gmail.com>
2020-08-21 15:10:34 +0900

    lol 컴포넌트 추가

--- src/pages/lol.js
+++ src/pages/lol.js
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import { Top } from '../components/top'
 import '../components/top/index.scss'
+import '../styles/lol.scss'
+import CountUp from 'react-countup'
 
 export default ({ data }) => {
-  console.log(data)
+  const [gameCount, setGameCount] = useState(0)
+  const [playTime, setPlayTime] = useState(0)
+  useEffect(() => {
+    axios.get('http://localhost:5000/getWeeklyData').then(res => {
+      console.log(res.data)
+      setPlayTime(res.data.currentPlayTime / 60)
+      setGameCount(res.data.currentCount)
+    })
+  }, [])
   return (
     <>
       <Top title={'즐겁게, 코드'} location={location} rootPath={'rootPath'} />
-      <div>
-        <p>이번 주의 롤 기록!</p>
-        <div></div>
+      <div className="container">
+        <div>I Love LOL!</div>
+        <div className="game-count">
+          <div className="content">
+            주간 플레이 판수 :{' '}
+            <CountUp end={gameCount} start={0} duration={5} /> 판
+          </div>
+        </div>
+        <div className="playtime">
+          <div className="content">
+            주간 플레이 시간 : <CountUp end={playTime} /> 분
+          </div>
+        </div>
+        <div>지난주 대비 플레이타임 증가율 : 22%</div>
+        <div>지난주 대비 판수 증가율 : 14%</div>
       </div>
     </>
   )
