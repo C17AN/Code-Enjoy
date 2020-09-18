a7b087dd06ee599a933a924b4721906014b8ea89
c17an <kimchanmin1@gmail.com>
2020-09-18 16:50:37 +0900

    About 페이지 추가

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -7,9 +7,12 @@ import * as Lang from '../constants'
 import '../components/top/index.scss'
 
 export default ({ data, location }) => {
-  const resumes = data.allMarkdownRemark.edges
+  const pages = data.allMarkdownRemark.edges
+  const projects = pages
+    .filter(({ node }) => node.frontmatter.title === 'projects')
+    .map(({ node }) => node)[0]
 
-  const resume = resumes
+  const about = pages
     .filter(({ node }) => node.frontmatter.title === 'about')
     .map(({ node }) => node)[0]
 
@@ -20,13 +23,22 @@ export default ({ data, location }) => {
         style={{
           marginLeft: `auto`,
           marginRight: `auto`,
-          maxWidth: rhythm(24),
+          maxWidth: rhythm(50),
           padding: `${rhythm(0.5)} ${rhythm(3 / 4)} ${rhythm(1.5)} ${rhythm(
             3 / 4
           )}`,
         }}
       >
-        <div dangerouslySetInnerHTML={{ __html: resume.html }} />
+        <div style={{ display: `flex`, justifyContent: `space-between` }}>
+          <div
+            dangerouslySetInnerHTML={{ __html: about.html }}
+            style={{ maxWidth: rhythm(25) }}
+          />
+          <div
+            dangerouslySetInnerHTML={{ __html: projects.html }}
+            style={{ maxWidth: rhythm(25) }}
+          />
+        </div>
       </div>
     </>
   )
