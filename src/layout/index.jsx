4062fb263cfd05744622aefb80a2add3881e59cd
김찬민 <chanmin@gimchanmin-ui-MacBook-Pro.local>
2020-11-16 19:03:16 +0900

    styled component 적용

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import { Top } from '../components/top'
 import { Header } from '../components/header'
@@ -8,27 +9,29 @@ import { rhythm } from '../utils/typography'
 
 import './index.scss'
 
+const Wrapper = styled.div`
+  max-width: ${rhythm(30)};
+  padding: ${rhythm(1.5)} ${rhythm(3 / 4)};
+  margin-left: ${props => (props.isPost ? 'auto' : '25rem')};
+  margin-right: ${props => (props.isPost ? 'auto' : '0rem')};
+  @media (max-width: 768px) {
+    margin-left: auto;
+    margin-right: auto;
+  }
+`
+
 export const Layout = ({ location, title, children, isPost }) => {
   const rootPath = `${__PATH_PREFIX__}/`
 
   return (
     <React.Fragment>
       <Top title={title} location={location} rootPath={rootPath} />
-      <div
-        style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-          maxWidth: rhythm(30),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-          marginLeft: isPost ? `auto` : `25rem`,
-          marginRight: isPost ? `auto` : `0rem`,
-        }}
-      >
+      <Wrapper>
         <ThemeSwitch />
         <Header title={title} location={location} rootPath={rootPath} />
         {children}
         <Footer />
-      </div>
+      </Wrapper>
     </React.Fragment>
   )
 }
